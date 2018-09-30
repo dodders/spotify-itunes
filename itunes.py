15f689186b52f48665a8f34eececede709e7859c
georged <6376745+dodders@users.noreply.github.com>
2018-09-30 17:56:44 -0400

    working playlist extract.

--- itunes.py
+++ itunes.py
@@ -1,34 +1,49 @@
 import untangle
-import pprint
+import json
 
 obj = untangle.parse('data/imac.itunes.xml')
+includef = open('data/playlist-include.txt', 'r')
+dolist = json.loads(includef.read())
+print('process list: ', dolist)
 
 
 def get_playlists():
-    ret = []
+    ret = {}
     plists = obj.plist.dict.array.dict[10:]
     for plist in plists:
-        ret.append(plist.string[0].cdata)
+        pname = plist.string[0].cdata
+        if pname in dolist:
+            print('adding playlist ', pname)
+            playlist = {}
+            playlist['name'] = pname
+            tracks = []
+            for id in plist.array.dict:
+                tracks.append(id.integer.cdata)
+            playlist['tracks'] = tracks
+            ret[playlist['name']] = playlist
+        else:
+            print('skipping playlist ', pname)
+
     return ret
 
 
 def get_tracks():
     ret = {}
     tracks = obj.plist.dict.dict.dict
-    for track in tracks[:100]:
+    for track in tracks:
         id = track.integer[0].cdata
         t = {'id': id,
              'name': track.string[0].cdata,
              'artist': track.string[1].cdata,
-             'album': track.string[2].cdata,
-             'year': track.integer[7].cdata}
+             'album': track.string[2].cdata}
         ret[id] = t
     return ret
 
 
 if __name__ == '__main__':
-    # lists = get_playlists()
-    # print(lists)
+    lists = get_playlists()
+    with open('data/playlists.txt', 'w') as f:
+        f.write(json.dumps(lists))
     tracks = get_tracks()
-    pprint.pprint(tracks)
-    print('track 2084 is', tracks['2084'])
+    with open('data/tracks.txt', 'w') as f:
+        f.write(json.dumps(tracks))
